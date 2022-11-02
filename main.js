6b07b8a2f7f5122186421efd28af87c602884350
jiemarabanzado <jiemar.abanzado@yahoo.it>
2022-11-02 15:42:54 +0100

    consegna finita+bonus

--- main.js
+++ main.js
@@ -1,39 +1,50 @@
-
+//default behavior
 let inside=document.querySelector('.square');
 for (let index = 1; index <=100; index++) {
     let element=document.createElement('div');
     element.innerHTML=index;
+    element.addEventListener('click',function(){
+        let number=element.innerHTML;
+        console.log(number);
+        element.classList.add('checked');
+    });
     inside.append(element);
     element.classList.add('square10');
     
 }
+
+//functions
 function Difficulty(level){
     inside.innerHTML='';
-    if(level=='Easy'){
-        for (let index = 1; index <=100; index++) {
-            let element=document.createElement('div');
-            element.innerHTML=index;
-            inside.append(element);
-            element.classList.add('square10');   
+    for (let index = 1; index <=level; index++) {
+        let element=document.createElement('div');
+        element.innerHTML=index;
+        element.addEventListener('click', function(){
+            let number=element.innerHTML;
+            console.log(number);
+            element.classList.add('checked');
+        });
+        inside.append(element);
+        if(level==100){
+            element.classList.add('square10');    
+        }else if(level==81){
+            element.classList.add('square9');
         }
-    }else if(level=='Normal'){
-        for (let index = 1; index <=81; index++) {
-            let element=document.createElement('div');
-            element.innerHTML=index;
-            inside.append(element);
-            element.classList.add('square9');   
-        }
-    }else{
-        for (let index = 1; index <=49; index++) {
-            let element=document.createElement('div');
-            element.innerHTML=index;
-            inside.append(element);
-            element.classList.add('square7');   
+        else{
+            element.classList.add('square7');
         }
     }
 }
-
 let choose=document.getElementById('GetDifficulty').addEventListener('click',function(){
     let choice=document.getElementById('difficulty').value;
-    Difficulty(choice);
+    if(choice=='Easy'){
+        choice=100;
+        Difficulty(choice);
+    }else if(choice=='Normal'){
+        choice=81;
+        Difficulty(choice);
+    }else{
+        choice=49;
+        Difficulty(choice);
+    }
 });
